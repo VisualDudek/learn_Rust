0053837d45bb0c639bfd5a775781abe12008ad3a
MarcinDudek <dudek.v@gmail.com>
2026-08-26 22:28:57 +0200

    add Vec move exercises

--- my_rustlings/002_vec/src/main.rs
+++ my_rustlings/002_vec/src/main.rs
@@ -1,15 +1,24 @@
-
-// --- SOLUTION ---
-
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let vec = vec;
 
     vec.push(88);
 
     vec
 }
 
+fn fill_vec_better(vec: Vec<i32>) -> Vec<i32> {
+    // TODO: you do not need following line
+    let vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+fn vec_from_slice(arr: &[i32]) -> Vec<i32> {
+    Vec::from(arr)
+}
 
 fn vec_form_vec_ref(v: &Vec<i32>) -> Vec<i32> {
     // Vec::from(v)
@@ -28,6 +37,13 @@ mod tests {
     fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
         let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+
+    #[test]
+    fn move_semantics2() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec_better(vec0);
         assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
---
+++ my_rustlings/solutions/002_vec/src/main.rs
@@ -0,0 +1,79 @@
+// --- SOLUTION ---
+#![allow(dead_code)]
+// TODO: Fix the compiler error in this function.
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+fn fill_vec_better(mut vec: Vec<i32>) -> Vec<i32> {
+    // you do not need following line
+    // let vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+fn vec_from_slice(v: &[i32]) -> Vec<i32> {
+    //               ^^^^^^ tutaj się dzieją ciekawe rzeczy przy przekazywaniu &Vec<i32>
+    Vec::from(v)
+}
+
+fn vec_form_vec_ref(v: &Vec<i32>) -> Vec<i32> {
+    // Vec::from(v) <-- dlaczego to nie działa ?
+    v.to_vec()
+}
+
+// brakuje mut in-place przez ref
+fn vec_mut_inplace(v: &mut Vec<i32>) {
+    v.push(88);
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn move_semantics1() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+
+    #[test]
+    fn move_semantics2() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec_better(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+
+    #[test]
+    fn test_vec_from_slice() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = vec_from_slice(&vec0);
+        assert_eq!(vec1, vec![22, 44, 66]);
+    }
+
+    #[test]
+    fn test_vec_from_vec_ref() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = vec_form_vec_ref(&vec0);
+        assert_eq!(vec1, vec![22, 44, 66]);
+    }
+
+    #[test]
+    fn test_vec_mut_inplace() {
+        let mut vec0 = vec![22, 44, 66];
+        vec_mut_inplace(&mut vec0);
+        assert_eq!(vec0, vec![22, 44, 66, 88]);
+    }
+
+}
